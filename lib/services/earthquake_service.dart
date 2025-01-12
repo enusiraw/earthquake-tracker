b6bffd9baa8b574acaaef71f80ca6d256743975a
enusiraw <edlsiraw@gmail.com>
2025-01-12 08:59:01 -0900

    updated

--- lib/services/earthquake_service.dart
+++ lib/services/earthquake_service.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class EarthquakeService {
   static const String baseUrl =
       'https://earthquake.usgs.gov/fdsnws/event/1/query';
 
-  Future<List<dynamic>> fetchEarthquakes(
-      {String startTime = '',
-      String endTime = '',
-      double minMagnitude = 4.0}) async {
+  Future<List<dynamic>> fetchEarthquakes({
+    String startTime = '',
+    String endTime = '',
+    double minMagnitude = 3.5,
+  }) async {
     final url = Uri.parse(
         '$baseUrl?format=geojson&starttime=$startTime&endtime=$endTime&minmagnitude=$minMagnitude');
 
@@ -16,7 +18,12 @@ class EarthquakeService {
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      return data['features']; // Return the list of earthquakes
+
+      if (data['features'] != null) {
+        return data['features'];
+      } else {
+        throw Exception('No earthquake data available.');
+      }
     } else {
       throw Exception('Failed to fetch earthquake data');
     }
