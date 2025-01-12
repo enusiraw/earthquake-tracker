bc51bdd47cbd0b2dacf111c5df5f798747b7dede
enusiraw <edlsiraw@gmail.com>
2025-01-12 08:45:27 -0900

    updated

--- lib/model/earthquake.dart
+++ lib/model/earthquake.dart
@@ -1,21 +1,30 @@
 class Earthquake {
-  final String id;
   final String place;
   final double magnitude;
   final DateTime time;
+  final String countryCode;
 
-  Earthquake(
-      {required this.id,
-      required this.place,
-      required this.magnitude,
-      required this.time});
+  Earthquake({
+    required this.place,
+    required this.magnitude,
+    required this.time,
+    required this.countryCode,
+  });
 
   factory Earthquake.fromJson(Map<String, dynamic> json) {
+    final place = json['properties']['place'] ?? 'Unknown place';
+    final magnitude = json['properties']['mag'] != null
+        ? json['properties']['mag'].toDouble()
+        : 0.0;
+    final time =
+        DateTime.fromMillisecondsSinceEpoch(json['properties']['time']);
+    final countryCode = json['properties']['countryCode'] ?? 'Unknown';
+
     return Earthquake(
-      id: json['id'],
-      place: json['properties']['place'] ?? 'Unknown',
-      magnitude: (json['properties']['mag'] as num).toDouble(),
-      time: DateTime.fromMillisecondsSinceEpoch(json['properties']['time']),
+      place: place,
+      magnitude: magnitude,
+      time: time,
+      countryCode: countryCode,
     );
   }
 }
