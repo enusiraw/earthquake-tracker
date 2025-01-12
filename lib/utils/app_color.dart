965bbdafe7f27bb4ec89090bdf2c90adbb93ab8b
enusiraw <edlsiraw@gmail.com>
2025-01-12 09:00:03 -0900

    app color added

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF5D414D); 
+  static const Color secondary = Color(0xFFABD4C1); 
+  static const Color accent = Color(0xFFABD4C1); 
+  static const Color background = Color(0xFFE5F6C6); 
+}
