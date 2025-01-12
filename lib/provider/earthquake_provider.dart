3ddba9702d258c16a0ffb958af8ff80032c11cc6
enusiraw <edlsiraw@gmail.com>
2025-01-12 02:31:20 -0900

    Modify provider to fetch earthquake data from the last 2 weeks to the current time

--- lib/provider/earthquake_provider.dart
+++ lib/provider/earthquake_provider.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../model/earthquake.dart';
 import '../services/earthquake_service.dart';
 
 final earthquakeProvider = FutureProvider<List<Earthquake>>((ref) async {
   final service = EarthquakeService();
+  final currentDate = DateTime.now();
+  final twoWeeksAgo = currentDate.subtract(const Duration(days: 14));
+  final endTime =
+      "${currentDate.year}-${currentDate.month.toString().padLeft(2, '0')}-${currentDate.day.toString().padLeft(2, '0')}";
+  final startTime =
+      "${twoWeeksAgo.year}-${twoWeeksAgo.month.toString().padLeft(2, '0')}-${twoWeeksAgo.day.toString().padLeft(2, '0')}";
+
+
   final data = await service.fetchEarthquakes(
-    startTime: '2025-01-01',
-    endTime: '2025-01-10',
+    startTime: startTime,
+    endTime: endTime,
     minMagnitude: 4.0,
   );
 
