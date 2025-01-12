8590b62e36c17c858cbbc7cb2473ab340fbd7b19
enusiraw <edlsiraw@gmail.com>
2025-01-12 09:01:05 -0900

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
+import 'package:earth_quake/utils/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Earthquake Tracker',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: AppColors.accent,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: AppColors.secondary),
+          bodyMedium: TextStyle(color: AppColors.accent),
+        ),
+      ),
+      home: HomeScreen(),
     );
   }
 }
-
